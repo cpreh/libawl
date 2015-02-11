fd439b33948b8dd3c4c6568b720d09c0c834032a
Carl Philipp Reh <carlphilippreh@gmail.com>
2015-02-11 14:57:59 +0100

    Update strong_typedef_construct usage in X11

--- src/backends/x11/cursor/convert_predefined.cpp
+++ src/backends/x11/cursor/convert_predefined.cpp
@@ -3,6 +3,7 @@
 #include <awl/cursor/predefined.hpp>
 #include <fcppt/strong_typedef_construct_cast.hpp>
 #include <fcppt/assert/unreachable.hpp>
+#include <fcppt/cast/to_unsigned_fun.hpp>
 #include <fcppt/config/external_begin.hpp>
 #include <X11/cursorfont.h>
 #include <fcppt/config/external_end.hpp>
@@ -20,28 +21,32 @@ awl::backends::x11::cursor::convert_predefined(
 	case awl::cursor::predefined::arrow:
 		return
 			fcppt::strong_typedef_construct_cast<
-				awl::backends::x11::cursor::shape
+				awl::backends::x11::cursor::shape,
+				fcppt::cast::to_unsigned_fun
 			>(
 				XC_left_ptr
 			);
 	case awl::cursor::predefined::cross:
 		return
 			fcppt::strong_typedef_construct_cast<
-				awl::backends::x11::cursor::shape
+				awl::backends::x11::cursor::shape,
+				fcppt::cast::to_unsigned_fun
 			>(
 				XC_cross
 			);
 	case awl::cursor::predefined::hand:
 		return
 			fcppt::strong_typedef_construct_cast<
-				awl::backends::x11::cursor::shape
+				awl::backends::x11::cursor::shape,
+				fcppt::cast::to_unsigned_fun
 			>(
 				XC_hand1
 			);
 	case awl::cursor::predefined::icon:
 		return
 			fcppt::strong_typedef_construct_cast<
-				awl::backends::x11::cursor::shape
+				awl::backends::x11::cursor::shape,
+				fcppt::cast::to_unsigned_fun
 			>(
 				XC_icon
 			);
