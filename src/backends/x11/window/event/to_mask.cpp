368f3859f898f33aec8df0648f094b32eaec33cc
Carl Philipp Reh <carlphilippreh@gmail.com>
2016-08-12 13:22:30 +0200

    Fix x11::window::event::to_mask

--- src/backends/x11/window/event/to_mask.cpp
+++ src/backends/x11/window/event/to_mask.cpp
@@ -2,10 +2,8 @@
 #include <awl/backends/x11/window/event/optional_mask_bit.hpp>
 #include <awl/backends/x11/window/event/to_mask.hpp>
 #include <awl/backends/x11/window/event/type.hpp>
-#include <fcppt/text.hpp>
-#include <fcppt/assert/unreachable_message.hpp>
 #include <fcppt/config/external_begin.hpp>
-#include <X11/Xlib.h>
+#include <X11/X.h>
 #include <fcppt/config/external_end.hpp>
 
 
@@ -66,7 +64,23 @@ case type_value: \
 			FocusChangeMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
-			MapNotify,
+			KeymapNotify,
+			KeymapStateMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			Expose,
+			ExposureMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			VisibilityNotify,
+			VisibilityChangeMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			CreateNotify,
+			SubstructureNotifyMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			DestroyNotify,
 			StructureNotifyMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
@@ -74,25 +88,58 @@ case type_value: \
 			StructureNotifyMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
-			ResizeRequest,
-			ResizeRedirectMask
+			MapNotify,
+			StructureNotifyMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
-			ConfigureRequest,
+			MapRequest,
 			SubstructureRedirectMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			ReparentNotify,
+			StructureNotifyMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
 			ConfigureNotify,
 			StructureNotifyMask
 		);
 		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
-			DestroyNotify,
+			ConfigureRequest,
+			SubstructureRedirectMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			GravityNotify,
 			StructureNotifyMask
 		);
-	case GenericEvent:
-		FCPPT_ASSERT_UNREACHABLE_MESSAGE(
-			FCPPT_TEXT("Don't register generic events for a window!")
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			ResizeRequest,
+			ResizeRedirectMask
 		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			CirculateNotify,
+			StructureNotifyMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			CirculateRequest,
+			SubstructureRedirectMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			PropertyNotify,
+			PropertyChangeMask
+		);
+		AWL_BACKENDS_X11_WINDOW_EVENT_TO_MASK_CASE(
+			ColormapNotify,
+			ColormapChangeMask
+		);
+	case GraphicsExpose:
+	case NoExpose:
+	case SelectionClear:
+	case SelectionRequest:
+	case SelectionNotify:
+	case ClientMessage:
+	case MappingNotify:
+	case GenericEvent:
+		break;
 	}
 
 	return
