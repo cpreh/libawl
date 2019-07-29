4f615c79416b40051bfc753cb0dd2df78d8bd0e3
Carl Philipp Reh <carlphilippreh@gmail.com>
2019-07-29 11:43:54 +0200

    Added x11::window::delete_property

---
+++ include/awl/backends/x11/window/delete_property.hpp
@@ -0,0 +1,30 @@
+#ifndef AWL_BACKENDS_X11_WINDOW_DELETE_PROPERTY_HPP_INCLUDED
+#define AWL_BACKENDS_X11_WINDOW_DELETE_PROPERTY_HPP_INCLUDED
+
+#include <awl/backends/x11/window/base_fwd.hpp>
+#include <awl/backends/x11/window/property_fwd.hpp>
+#include <awl/detail/symbol.hpp>
+
+
+namespace awl
+{
+namespace backends
+{
+namespace x11
+{
+namespace window
+{
+
+AWL_DETAIL_SYMBOL
+void
+delete_property(
+	awl::backends::x11::window::base const &,
+	awl::backends::x11::window::property
+);
+
+}
+}
+}
+}
+
+#endif
---
+++ src/backends/x11/window/delete_property.cpp
@@ -0,0 +1,22 @@
+#include <awl/backends/x11/display.hpp>
+#include <awl/backends/x11/window/base.hpp>
+#include <awl/backends/x11/window/delete_property.hpp>
+#include <awl/backends/x11/window/property.hpp>
+#include <fcppt/config/external_begin.hpp>
+#include <X11/Xlib.h>
+#include <fcppt/config/external_end.hpp>
+
+
+void
+awl::backends::x11::window::delete_property(
+	awl::backends::x11::window::base const &_window,
+	awl::backends::x11::window::property const _property
+)
+{
+	// Always returns 1
+	XDeleteProperty(
+		_window.display().get(),
+		_window.get(),
+		_property.get().get()
+	);
+}
