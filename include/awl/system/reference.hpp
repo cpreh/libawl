74f2ccd0c4f2cf63734540c82f2f13d8701c6f88
Carl Philipp Reh <carlphilippreh@gmail.com>
2020-05-17 10:39:32 +0200

    Added system::reference

---
+++ include/awl/system/reference.hpp
@@ -0,0 +1,23 @@
+#ifndef AWL_SYSTEM_REFERENCE_HPP_INCLUDED
+#define AWL_SYSTEM_REFERENCE_HPP_INCLUDED
+
+#include <awl/system/object_fwd.hpp>
+#include <fcppt/reference_impl.hpp>
+
+
+namespace awl
+{
+namespace system
+{
+
+using
+reference
+=
+fcppt::reference<
+	awl::system::object
+>;
+
+}
+}
+
+#endif
