e30a5309eaa806834c4363c618326efc16ac37b5
Carl Philipp Reh <carlphilippreh@gmail.com>
2023-01-04 14:54:38 +0100

    Remove assert/error include

--- src/backends/windows/system/event/handle_holder.cpp
+++ src/backends/windows/system/event/handle_holder.cpp
@@ -2,7 +2,6 @@
 #include <awl/backends/windows/windows.hpp>
 #include <awl/backends/windows/system/event/handle_holder.hpp>
 #include <fcppt/text.hpp>
-#include <fcppt/assert/error.hpp>
 
 awl::backends::windows::system::event::handle_holder::handle_holder()
     : handle_(::CreateEvent(NULL, FALSE, FALSE, NULL))
