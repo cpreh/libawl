1737f20bef18ca7582a244673354b77cca099306
Carl Philipp Reh <carlphilippreh@gmail.com>
2023-07-30 16:13:07 +0200

    Update epoll::set

--- src/backends/linux/epoll/set.cpp
+++ src/backends/linux/epoll/set.cpp
@@ -22,35 +22,36 @@
 #include <chrono>
 #include <fcppt/config/external_end.hpp>
 
-awl::backends::linux::epoll::set::set() : epoll_fd_(), events_() {}
+awl::backends::linux::epoll::set::set() : epoll_fd_{}, events_{} {}
 
 awl::backends::linux::epoll::set::~set() = default;
 
 void awl::backends::linux::epoll::set::add(awl::backends::posix::fd const _fd)
 {
-  events_.push_back(epoll_event());
+  // NOLINTNEXTLINE(hicpp-use-emplace,modernize-use-emplace)
+  this->events_.push_back(epoll_event{});
 
   epoll_event ev{};
   ev.events = EPOLLIN;
   ev.data.fd = _fd.get();
 
-  awl::backends::linux::epoll::ctl(epoll_fd_, EPOLL_CTL_ADD, _fd, &ev);
+  awl::backends::linux::epoll::ctl(this->epoll_fd_, EPOLL_CTL_ADD, _fd, &ev);
 }
 
 void awl::backends::linux::epoll::set::remove(awl::backends::posix::fd const _fd)
 {
-  awl::backends::linux::epoll::ctl(epoll_fd_, EPOLL_CTL_DEL, _fd, nullptr);
+  awl::backends::linux::epoll::ctl(this->epoll_fd_, EPOLL_CTL_DEL, _fd, nullptr);
 
-  events_.pop_back();
+  this->events_.pop_back();
 }
 
 awl::backends::linux::epoll::fd_vector awl::backends::linux::epoll::set::epoll(
     awl::backends::posix::optional_duration const &_opt_duration)
 {
-  int const ret(::epoll_wait(
-      epoll_fd_.get().get(),
-      events_.data(),
-      fcppt::cast::size<int>(fcppt::cast::to_signed(events_.size())),
+  int const ret{::epoll_wait(
+      this->epoll_fd_.get().get(),
+      this->events_.data(),
+      fcppt::cast::size<int>(fcppt::cast::to_signed(this->events_.size())),
       fcppt::optional::maybe(
           _opt_duration,
           fcppt::const_(-1),
@@ -66,20 +67,20 @@ awl::backends::linux::epoll::fd_vector awl::backends::linux::epoll::set::epoll(
                       fcppt::output_to_fcppt_string(_duration.count()) +
                       FCPPT_TEXT(" is too large for epoll_wait")};
                 });
-          })));
+          }))};
 
   if (ret == -1)
   {
     throw awl::exception{FCPPT_TEXT("epoll_wait failed!")};
   }
 
-  unsigned const ready(fcppt::cast::to_unsigned(ret));
+  unsigned const ready{fcppt::cast::to_unsigned(ret)};
 
   return fcppt::algorithm::map_optional<awl::backends::linux::epoll::fd_vector>(
       fcppt::make_int_range_count(ready),
       [this](unsigned const _index)
       {
-        epoll_event const &event(this->events_[_index]);
+        epoll_event const &event{this->events_[_index]};
 
         return fcppt::optional::make_if(
             (event.events & EPOLLIN) != 0,
