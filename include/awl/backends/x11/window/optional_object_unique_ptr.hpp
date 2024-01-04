7a71627d2d14c5300b450782d91f81cb53b8944b
Carl Philipp Reh <carlphilippreh@gmail.com>
2024-01-04 13:31:37 +0100

    Remove obsolete header files

--- include/awl/backends/x11/window/optional_object_unique_ptr.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef AWL_BACKENDS_X11_WINDOW_OPTIONAL_OBJECT_UNIQUE_PTR_HPP_INCLUDED
-#define AWL_BACKENDS_X11_WINDOW_OPTIONAL_OBJECT_UNIQUE_PTR_HPP_INCLUDED
-
-#include <awl/backends/x11/window/optional_object_unique_ptr_fwd.hpp> // IWYU pragma: export
-#include <fcppt/optional/object_impl.hpp> // IWYU pragma: keep
-
-#endif
